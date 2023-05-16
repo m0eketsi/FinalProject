8c2033ecc908b0f2716a1a1da036f89e6c770da1
Moeketsi Moloi <moloimason@gmail.com>
2023-05-16 11:32:36 -0700

    update

--- One Life/Assets/Scripts/LaserAim.cs
+++ One Life/Assets/Scripts/LaserAim.cs
@@ -5,23 +5,36 @@ using UnityEngine;
 public class LaserAim : MonoBehaviour
 {
     public LineRenderer lineRenderer;
+    Ray ray;
+    RaycastHit hit;
+    public GameObject muzzle;
+    public GameObject testBlock;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    /*void Update()
+    void Update()
     {
-    lineRenderer.useWorldSpace = false;
-    RaycastHit hit;
-    Physics.Raycast(transform.position,transform.forward, hit.point);
-    if(hit.collider){
-    lineRenderer.SetPosition(1,Vector3(0,0,hit.distance));
+        projectLaser();
+        detectHit();
     }
-    else{
-    lineRenderer.SetPosition(1,Vector3(0,0,5000));
+    void projectLaser()
+    {
+        ray = new Ray(muzzle.transform.position, transform.forward * 5000);
+        Debug.DrawRay(muzzle.transform.position, transform.forward * 5000, Color.green);
+        lineRenderer.SetPosition(0, muzzle.transform.position);
     }
-    }*/
+    void detectHit()
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider)
+            {
+            lineRenderer.SetPosition(1, hit.point);
+            }
+        } 
+    }
 }
